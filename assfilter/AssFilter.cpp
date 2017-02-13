64eefbb01ba1d681f627d1532a267b49866caa2e
Blitzker <blitzker99@gmail.com>
2017-02-13 15:58:00 -0500

    Release version 0.3

--- assfilter/AssFilter.cpp
+++ assfilter/AssFilter.cpp
@@ -37,7 +37,7 @@ AssFilter::AssFilter(LPUNKNOWN pUnk, HRESULT* pResult)
     m_renderer = decltype(m_renderer)(ass_renderer_init(m_ass.get()));
 
     m_stringOptions["name"] = L"AssFilterMod";
-    m_stringOptions["version"] = L"0.2.1.0";
+    m_stringOptions["version"] = L"0.3.0.0";
     m_stringOptions["yuvMatrix"] = L"None";
     m_stringOptions["outputLevels"] = L"PC";
     m_boolOptions["combineBitmaps"] = false;
